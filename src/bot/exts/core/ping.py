7b6d5f216d14081c8387f19af48119af1eacf9b0
Robin5605 <robinjefferson123@gmail.com>
2023-01-26 22:02:20 -0600

    Setup app commands and basic ping command

--- src/bot/exts/core/ping.py
+++ src/bot/exts/core/ping.py
@@ -1,6 +1,6 @@
 """Ping cog"""
 
-from discord import Embed
+from discord import Embed, Interaction, app_commands
 from discord.ext import commands
 
 from bot.bot import Bot
@@ -13,16 +13,25 @@ class Ping(commands.Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
 
-    @commands.command(name="ping")
-    async def ping(self, ctx: commands.Context) -> None:
-        """Ping the bot to see its latency and state."""
+    def _build_ping_embed(self) -> Embed:
         embed = Embed(
             title=":ping_pong: Pong!",
             colour=Colours.bright_green,
             description=f"Gateway Latency: {round(self.bot.latency * 1000)}ms",
         )
 
+        return embed
+
+    @commands.command(name="ping")
+    async def ping(self, ctx: commands.Context) -> None:
+        """Ping the bot to see its latency and state."""
+        embed = self._build_ping_embed() 
         await ctx.send(embed=embed)
+
+    @app_commands.command(name="ping", description="Get gateway latency")
+    async def ping_app_command(self, interaction: Interaction) -> None:
+        embed = self._build_ping_embed()
+        await interaction.response.send_message(embed=embed)
 
 
 async def setup(bot: Bot) -> None:
